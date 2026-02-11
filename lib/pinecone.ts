9a73abe38786ac83db0bf09d121c89b17b67ec4b
nancy-kataria <katarianancy8@gmail.com>
2026-02-11 13:11:50 -0800

    pinecone config

---
+++ lib/pinecone.ts
@@ -0,0 +1,21 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+const apiKey = process.env.PINECONE_API_KEY;
+const indexName = process.env.PINECONE_INDEX_NAME;
+
+if (!apiKey) {
+  throw new Error("PINECONE_API_KEY is not set in environment variables");
+}
+
+if (!indexName) {
+  throw new Error("PINECONE_INDEX_NAME is not set in environment variables");
+}
+
+export const pinecone = new Pinecone({
+  apiKey,
+});
+
+export const index = pinecone.Index(indexName);
