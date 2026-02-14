6a65aafb7e336af44a2de2ee946c4cfd4ae83ef2
nancy-kataria <katarianancy8@gmail.com>
2026-02-13 16:42:17 -0800

    removing explicit dotenv processing

--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -1,7 +1,4 @@
 import { Pinecone } from "@pinecone-database/pinecone";
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.local' });
 
 const apiKey = process.env.PINECONE_API_KEY;
 const indexName = process.env.PINECONE_INDEX_NAME;
